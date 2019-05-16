ba80a7b7d8be4029c5cec02182faa364ac47bc02
eipc16 <pprzemek.312@gmail.com>
2019-05-16 18:06:04 +0200

    Working host

--- WCF-Streams-Host/Program.cs
+++ WCF-Streams-Host/Program.cs
@@ -1,21 +1,47 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
+using WCF_Streams;
 
 namespace WCF_Streams_Host
 {
     class Program
     {
+
+        public static string BASE_NAME = "MainService";
+        public static int PORT = 9005;
+
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Uri baseAddress = new Uri($"http://localhost:{PORT}/{BASE_NAME}");
+            ServiceHost host = new ServiceHost(typeof(MainService), baseAddress);
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+            try
+            {
+                ServiceEndpoint endpoint = host.AddServiceEndpoint(typeof(MainServiceInterface), new BasicHttpBinding(), "default_endpoint");
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior
+                {
+                    HttpGetEnabled = true
+                };
+                host.Description.Behaviors.Add(smb);
+
+                host.Open();
+                Console.WriteLine($"Service {BASE_NAME} has connected successfully and started working");
+                Console.WriteLine("Press <ENTER> to finish!");
+                Console.ReadLine();
+       
+                host.Close();
+                Console.WriteLine($"Service {BASE_NAME} has finished working!");
+
+            } catch (CommunicationException exception)
+            {
+                Console.WriteLine($"Exception encountered: {exception}");
+                host.Abort();
+            }
         }
     }
 }
