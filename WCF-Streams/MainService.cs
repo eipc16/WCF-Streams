b8d2117fce6637f00a7ce7ecfb7b4e0004e90e1d
eipc16 <pprzemek.312@gmail.com>
2019-05-16 17:29:14 +0200

    Added service contract

---
+++ WCF-Streams/MainService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+using System.Threading;
+
+namespace WCF_Streams
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    [ServiceBehavior(ConcurrencyMode = ConcurrencyMode.Multiple)]
+    public class MainService : MainServiceInterface
+    {
+        public void ASyncFunction(string data)
+        {
+            Console.Write("...{0}: Asynchronous function: START");
+            Thread.Sleep(3000);
+            Console.Write("...{0}: Asynchronous function: STOP");
+            return;
+        }
+
+        public void SyncFunction(string data)
+        {
+            Console.Write("...{0}: Synchronous function: START");
+            Thread.Sleep(3000);
+            Console.Write("...{0}: Synchronous function: STOP");
+            return;
+        }
+    }
+}
---
+++ WCF-Streams/MainServiceInterface.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WCF_Streams
+{
+    [ServiceContract]
+    public interface MainServiceInterface
+    {
+        [OperationContract]
+        void SyncFunction(String data);
+
+        [OperationContract(IsOneWay = true)]
+        void ASyncFunction(String data);
+
+    }
+}
